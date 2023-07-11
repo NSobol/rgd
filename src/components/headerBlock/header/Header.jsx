6f133dafa64354dda465cd01cac5ae1ba2c19d8d
Soboleva Natalia <nsoboleva1979@rambler.ru>
2023-07-11 21:55:58 +0300

    Добавление секции поиска

--- src/components/headerBlock/header/Header.jsx
+++ src/components/headerBlock/header/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Logo } from '../logo/Logo';
 import { Menu } from '../menu/Menu';
 import s from './header.module.css';
+import { Search } from '../search/Search';
 
 export const Header = () => {
   return (
     <div className={s['header']} id='header'>
-        <Logo />
-        <Menu />
+      <Logo />
+      <Menu />
+      <Search />
     </div>
   );
 };
