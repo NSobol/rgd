bc14dc0c7763c1e159d38991c87f2d80f3fe6ea4
Soboleva Natalia <nsoboleva1979@rambler.ru>
2023-07-17 07:08:43 +0300

    Добавление вкомпоненте

--- src/components/personDataForm/PersonDataForm.jsx
+++ src/components/personDataForm/PersonDataForm.jsx
@@ -4,31 +4,42 @@ export const PersonDataForm = () => {
   return (
     <div>
       <form>
-        <h2>Персональные данные</h2>
-        <div className='form-group'>
-          <div className='form-group-item'>
-            <label htmlFor='surname'>Фамилия</label>
-            <input type='text' name='surname' id='surname' required />
+        <div className='personal'>
+          <h2>Персональные данные</h2>
+          <div className='personal-form-group'>
+            <div className='personal-form-group-item'>
+              <label htmlFor='surname'>Фамилия</label>
+              <input type='text' name='surname' id='surname' required />
+            </div>
+            <div className='personal-form-group-item'>
+              <label htmlFor='name'>Имя</label>
+              <input type='text' name='name' id='name' required />
+            </div>
+            <div className='personal-form-group-item'>
+              <label htmlFor='middleName'>Отчество</label>
+              <input type='text' name='middleName' id='middleName' required />
+            </div>
           </div>
-          <div className='form-group-item'>
-            <label htmlFor='name'>Имя</label>
-            <input type='text' name='name' id='name' required />
+          <div className='personal-form-group-item'>
+            <label htmlFor='tel'>Контактный телефон</label>
+            <input type='tel' name='tel' id='tel' required />
           </div>
-          <div className='form-group-item'>
-            <label htmlFor='middleName'>Отчество</label>
-            <input type='text' name='middleName' id='middleName' required />
+          <div className='personal-form-group-item'>
+            <label htmlFor='email'>E-mail</label>
+            <input type='email' name='email' id='email' required />
           </div>
         </div>
-        <div className='form-group-item'>
-          <label htmlFor='tel'>Контактный телефон</label>
-          <input type='tel' name='tel' id='tel' required />
-        </div>
-        <div className='form-group-item'>
-          <label htmlFor='email'>E-mail</label>
-          <input type='email' name='email' id='email' required />
-        </div>
+        <div>
+          <h2>Способ оплаты</h2>
+          <div>
+            <label htmlFor=''>Онлайн</label>
+            <input type='radio' />
 
-        <h2>Способ оплаты</h2>
+            <label htmlFor=''>Наличными</label>
+            <input type='radio' />
+          </div>
+        </div>
+        <button>КУПИТЬ БИЛЕТЫ</button>
       </form>
     </div>
   );
