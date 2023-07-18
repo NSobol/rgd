1b5df2b81dba385bceef121e933feb9686149063
Soboleva Natalia <nsoboleva1979@rambler.ru>
2023-07-18 03:24:13 +0300

    Добавлены страницы и компоненты

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css';
 import { MainPage } from './pages/mainPage/MainPage';
 import { LoadingSearchPage } from './pages/loadingSearchPage/LoadingSearchPage';
 import { SuccessfulOrderPage } from './pages/successfulOrderPage/SuccessfulOrderPage';
+import { PaymentPage } from './pages/paymentPage/PaymentPage';
+import { OrderConfirmationPage } from './pages/orderConfirmationPage/OrderConfirmationPage';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path='/' element={<MainPage />} />
         <Route path='/loading' element={<LoadingSearchPage />} />
         <Route path='/fulorder' element={<SuccessfulOrderPage />} />
+        <Route path='/payment' element={<PaymentPage />} />
+        <Route path='/confirm' element={<OrderConfirmationPage />} />
       </Routes>
     </div>
   );
